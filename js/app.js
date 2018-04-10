c1ed88ae2a9b8c6d789854b2d266e28c559b89f9
Jonathan Garman <jonjon@Taylors-MacBook-Pro-2.local>
2018-04-09 18:43:40 -0700

    second commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-alert ('running!')
+todo = document.getElementById("todo")
+
+
+
+document.getElementById("check").addEventListener("click", click);
+
+
+
+function click() {
+    list = document.getElementsByTagName("input")[0].value;
+    div = document.createElement("DIV")
+    del = document.createElement("BUTTON")
+    del.innerHTML = "Complete";
+    div.innerHTML = list;
+    todo.appendChild(div);
+    // document.getElementsByTagName("input")
+    div.appendChild(del);
+    
+    del.addEventListener("click", destroy);
+    function destroy(event) {
+        console.log(event.target)
+        event.target.parentNode.innerHTML= "";
+    }
+}
+
